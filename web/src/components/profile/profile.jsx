f5c7a324d96e9b69152613cce136b70366730ac0
Muhammad Ahad <120649081+ahadsts9901@users.noreply.github.com>
2023-10-22 19:31:08 +0500

    code

--- web/src/components/profile/profile.jsx
+++ web/src/components/profile/profile.jsx
@@ -226,9 +226,9 @@ const Profile = () => {
               userPosts.map((post, index) => (
 
                 (state.user.userId === profile.userId || state.isAdmin == true) ?
-                  (<UserPost key={index} title={post.title} text={post.text} time={post.time} postId={post._id} del={deletePost} edit={editPost} />)
+                  (<UserPost key={index} title={post.title} text={post.text} time={post.time} postId={post._id} del={deletePost} edit={editPost} likedBy={post.likes} />)
                   :
-                  (<Post key={index} title={post.title} text={post.text} time={post.time} postId={post._id} />)
+                  (<Post key={index} title={post.title} text={post.text} time={post.time} postId={post._id} likedBy={post.likes} />)
 
               ))
             ))}
