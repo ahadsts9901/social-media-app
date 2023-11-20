4ade7c74fcfa48d6df71618424bd210d2ea8d753
Muhammad Ahad <120649081+ahadsts9901@users.noreply.github.com>
2023-11-20 13:13:00 +0500

    games added

--- web/src/components/games/games.jsx
+++ web/src/components/games/games.jsx
@@ -1,23 +1,25 @@
-import React
-// , { useState, useRef }
- from 'react';
+import React from "react"; // , { useState, useRef }
 // import axios from 'axios';
 // import Swal from 'sweetalert2';
-import './games.css';
-import '../main.css'
+import "./games.css";
+import "../main.css";
 // import { Link, useNavigate } from 'react-router-dom';
 // import logo from "../assets/logoDark.png"
 
-import { baseUrl } from '../../core.mjs';
- 
-const Games = () => {
+import { baseUrl } from "../../core.mjs";
 
+const Games = () => {
   return (
-  <div className='test'>
-    <h2>Games</h2>
-    <h2>Under Construction</h2>
-    </div>  
-  )
+    <div className="test">
+      {/* <h2>Games</h2> */}
+      {/* <h2>Under Construction</h2> */}
+      <iframe
+        src="https://ahadsts9901.github.io/sma-games/"
+        className="gameWeb"
+        frameborder="0"
+      ></iframe>
+    </div>
+  );
 };
 
 export default Games;
